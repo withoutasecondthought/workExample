ae9761fd6d52ddb2f9f890e65b85105997cf1d6b
m3tamorphosis <mrmarkeld@gmail.com>
2022-05-26 16:17:32 +0500

    first and last commit just to give example of my code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './App.css'
+
+
+import MemeStartPage from "./screens/MemeStartPage";
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+} from "react-router-dom";
+import UsersScreen from "./screens/UsersScreen";
+import TodoScreen from "./screens/TodoScreen";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<MemeStartPage/>}/>
+            <Route path="/users" element={<UsersScreen/>}/>
+            <Route path="/todos" element={<TodoScreen/>}/>
+
+        </Routes>
+    </BrowserRouter>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
